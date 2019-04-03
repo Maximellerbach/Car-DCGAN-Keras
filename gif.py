3a0ee7c133faa8d39c6899a932addcc2859dd271
Maximellerbach <38923282+Maximellerbach@users.noreply.github.com>
2019-04-03 15:52:40 +0200

    Update gif.py

--- gif.py
+++ gif.py
@@ -2,9 +2,9 @@ import imageio
 from glob import glob
 from tqdm import tqdm
 
-filenames = glob('car\\*')
+filenames = glob('car\\*')#load training image
 
-with imageio.get_writer('gan\\training.gif', mode='I',duration=0.01) as writer:
+with imageio.get_writer('training.gif', mode='I',duration=0.01) as writer:
     for filename in tqdm(filenames):
         image = imageio.imread(filename)
         writer.append_data(image)
